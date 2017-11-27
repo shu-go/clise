926e54f1711a8c0771866e67c83b02529d862bfa
Shuhei Kubota <kubota.shuhei@gmail.com>
2017-11-27 23:06:26 +0900

    rename func FilterSwap->Filter, Filter->FilterSimpleFast

--- filter_inplace.go
+++ filter_inplace.go
@@ -18,6 +18,85 @@ func Filter(ptrSlice interface{}, funcs ...FilterFunc) {
 		return
 	}
 
+	movelist := make([]int, length)
+	okindex := 0
+
+	for i := 0; i < length; i++ {
+		allok := true
+		for _, f := range funcs {
+			if !f(i) {
+				allok = false
+			}
+		}
+		if allok {
+			movelist[i] = okindex
+			okindex++
+		} else {
+			movelist[i] = -1
+		}
+	}
+
+	if okindex == length {
+		return
+	}
+
+	swap := reflect.Swapper(rv.Elem().Interface())
+
+	for i, v := range movelist {
+		if v != -1 {
+			swap(i, v)
+		}
+	}
+
+	rv.Elem().SetLen(okindex)
+}
+
+// FilterSimple is simple example using MakeRemover.
+// This func removes elements one by one, not by range.
+func FilterSimple(ptrSlice interface{}, funcs ...FilterFunc) {
+	rv := reflect.ValueOf(ptrSlice)
+	if rv.Kind() != reflect.Ptr { // typeof ptrSlice != ptr
+		panic("not a pointer")
+	} else if rv.Elem().Kind() != reflect.Slice { // typeof *ptrSlice != []xxx
+		panic("not a pointer to a slice")
+	}
+
+	length := rv.Elem().Len() // len(*ptrSlice)
+	if length == 0 {
+		return
+	}
+
+	remove := MakeRemover(ptrSlice)
+
+	for i := length - 1; i >= 0; i-- {
+		allok := true
+		for _, f := range funcs {
+			if !f(i) {
+				allok = false
+			}
+		}
+		if allok {
+			continue
+		}
+
+		remove(i, i)
+	}
+}
+
+// FilterSimpleFast is the fast version of FilterSimple.
+func FilterSimpleFast(ptrSlice interface{}, funcs ...FilterFunc) {
+	rv := reflect.ValueOf(ptrSlice)
+	if rv.Kind() != reflect.Ptr { // typeof ptrSlice != ptr
+		panic("not a pointer")
+	} else if rv.Elem().Kind() != reflect.Slice { // typeof *ptrSlice != []xxx
+		panic("not a pointer to a slice")
+	}
+
+	length := rv.Elem().Len() // len(*ptrSlice)
+	if length == 0 {
+		return
+	}
+
 	remove := MakeRemover(ptrSlice)
 
 	rmlength := length / 2
@@ -58,163 +137,4 @@ func Filter(ptrSlice interface{}, funcs ...FilterFunc) {
 	if lastE != -1 {
 		remove(lastS, lastE)
 	}
-}
-
-// FilterSimple is basically same to Slice, with less memory usage and less speed in most cases.
-// This func removes elements one by one, not by range.
-func FilterSimple(ptrSlice interface{}, funcs ...FilterFunc) {
-	rv := reflect.ValueOf(ptrSlice)
-	if rv.Kind() != reflect.Ptr { // typeof ptrSlice != ptr
-		panic("not a pointer")
-	} else if rv.Elem().Kind() != reflect.Slice { // typeof *ptrSlice != []xxx
-		panic("not a pointer to a slice")
-	}
-
-	length := rv.Elem().Len() // len(*ptrSlice)
-	if length == 0 {
-		return
-	}
-
-	remove := MakeRemover(ptrSlice)
-
-	for i := length - 1; i >= 0; i-- {
-		allok := true
-		for _, f := range funcs {
-			if !f(i) {
-				allok = false
-			}
-		}
-		if allok {
-			continue
-		}
-
-		remove(i, i)
-	}
-}
-
-// FilterSwapSimple is slow and much safe version of Slice.
-func FilterSwapSimple(ptrSlice interface{}, funcs ...FilterFunc) {
-	rv := reflect.ValueOf(ptrSlice)
-	if rv.Kind() != reflect.Ptr { // typeof ptrSlice != ptr
-		panic("not a pointer")
-	} else if rv.Elem().Kind() != reflect.Slice { // typeof *ptrSlice != []xxx
-		panic("not a pointer to a slice")
-	}
-
-	length := rv.Elem().Len() // len(*ptrSlice)
-	if length == 0 {
-		return
-	}
-
-	swap := reflect.Swapper(rv.Elem().Interface())
-
-	ngboundary := length
-	for i := length - 1; i >= 0; i-- {
-		allok := true
-		for _, f := range funcs {
-			if !f(i) {
-				allok = false
-			}
-		}
-		if allok {
-			continue
-		}
-
-		if i < ngboundary-1 {
-			//swap(i, ngboundary-1) //not stable
-			for j := i; j < ngboundary-1; j++ {
-				swap(j, j+1)
-			}
-		}
-		ngboundary--
-	}
-
-	rv.Elem().SetLen(ngboundary)
-}
-
-func FilterSwap(ptrSlice interface{}, funcs ...FilterFunc) {
-	rv := reflect.ValueOf(ptrSlice)
-	if rv.Kind() != reflect.Ptr { // typeof ptrSlice != ptr
-		panic("not a pointer")
-	} else if rv.Elem().Kind() != reflect.Slice { // typeof *ptrSlice != []xxx
-		panic("not a pointer to a slice")
-	}
-
-	length := rv.Elem().Len() // len(*ptrSlice)
-	if length == 0 {
-		return
-	}
-
-	movelist := make([]int, length)
-	okindex := 0
-
-	for i := 0; i < length; i++ {
-		allok := true
-		for _, f := range funcs {
-			if !f(i) {
-				allok = false
-			}
-		}
-		if allok {
-			movelist[i] = okindex
-			okindex++
-		} else {
-			movelist[i] = -1
-		}
-	}
-
-	if okindex == length {
-		return
-	}
-
-	swap := reflect.Swapper(rv.Elem().Interface())
-
-	for i, v := range movelist {
-		if v != -1 {
-			swap(i, v)
-		}
-	}
-
-	rv.Elem().SetLen(okindex)
-}
-
-// FilterSwapUnstable is faster version of FilterSwap.
-// The order of slice may not be preserved.
-func FilterSwapUnstable(ptrSlice interface{}, funcs ...FilterFunc) {
-	rv := reflect.ValueOf(ptrSlice)
-	if rv.Kind() != reflect.Ptr { // typeof ptrSlice != ptr
-		panic("not a pointer")
-	} else if rv.Elem().Kind() != reflect.Slice { // typeof *ptrSlice != []xxx
-		panic("not a pointer to a slice")
-	}
-
-	length := rv.Elem().Len() // len(*ptrSlice)
-	if length == 0 {
-		return
-	}
-
-	swap := reflect.Swapper(rv.Elem().Interface())
-
-	ngboundary := length
-	for i := length - 1; i >= 0; i-- {
-		allok := true
-		for _, f := range funcs {
-			if !f(i) {
-				allok = false
-			}
-		}
-		if allok {
-			continue
-		}
-
-		if i < ngboundary-1 {
-			swap(i, ngboundary-1) //not stable
-			//for j := i; j < ngboundary-1; j++ {
-			//	swap(j, j+1)
-			//}
-		}
-		ngboundary--
-	}
-
-	rv.Elem().SetLen(ngboundary)
 }
