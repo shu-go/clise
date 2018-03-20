2ea4d008cbb72c68e9bda8ffbd5814620e6c5b92
Shuhei Kubota <kubota.shuhei@gmail.com>
2018-03-20 13:51:29 +0900

    add Find

---
+++ find.go
@@ -0,0 +1,38 @@
+package clise
+
+import (
+	"reflect"
+)
+
+func Find(slice interface{}, dest interface{}, funcs ...FilterFunc) bool {
+	rv := reflect.ValueOf(slice)
+	if rv.Kind() != reflect.Slice {
+		panic("not a slice")
+	}
+
+	dv := reflect.ValueOf(dest)
+	if dv.Kind() != reflect.Ptr {
+		panic("not a pointer")
+	}
+
+	if dv.Type().Elem() != rv.Type().Elem() { // typeof *dv != typeof rv[0]
+		panic("type not match")
+	}
+
+	length := rv.Len()
+
+	for i := 0; i < length; i++ {
+		allok := true
+		for _, f := range funcs {
+			if !f(i) {
+				allok = false
+			}
+		}
+		if allok {
+			dv.Elem().Set(rv.Index(i)) // *dv = rv[i]
+			return true
+		}
+	}
+
+	return false
+}
---
+++ find_test.go
@@ -0,0 +1,63 @@
+package clise
+
+import (
+	"testing"
+
+	"bitbucket.org/shu/gotwant"
+)
+
+func TestFind(t *testing.T) {
+	ss := []int{1, 2, 3}
+
+	var s int
+	found := Find(ss, &s)
+	gotwant.Test(t, found, true)
+	gotwant.Test(t, s, ss[0])
+
+	found = Find(ss, &s, func(i int) bool { return ss[i] == 2 })
+	gotwant.Test(t, found, true)
+	gotwant.Test(t, s, ss[1])
+
+	olds := s
+	found = Find(ss, &s, func(i int) bool { return ss[i] == 9 })
+	gotwant.Test(t, found, false)
+	gotwant.Test(t, s, olds)
+}
+
+func BenchmarkFind(b *testing.B) {
+	b.Run("clise", func(b *testing.B) {
+		ss := genIntSeq(sliceSize)
+		var s int
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			Find(ss, &s, func(j int) bool { return ss[j] == i })
+		}
+	})
+	b.Run("goway", func(b *testing.B) {
+		ss := genIntSeq(sliceSize)
+		var s int
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			findInt(ss, &s, func(j int) bool { return ss[j] == i })
+		}
+	})
+
+}
+
+func findInt(slice []int, dest *int, funcs ...func(i int) bool) bool {
+	for i := 0; i < len(slice); i++ {
+		allok := true
+		for _, f := range funcs {
+			if !f(i) {
+				allok = false
+			}
+		}
+		if !allok {
+			continue
+		}
+
+		*dest = slice[i]
+		return true
+	}
+	return false
+}
